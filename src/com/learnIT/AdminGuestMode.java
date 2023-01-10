c3affcc14fe075729599ce5adb33e69f841eb3be
wiwin ristanto <wiwinristanto94@gmail.com>
2023-01-10 17:58:14 +0700

    If Statement

---
+++ src/com/learnIT/AdminGuestMode.java
@@ -0,0 +1,28 @@
+package com.learnIT;
+
+import java.util.Scanner;
+
+public class AdminGuestMode {
+
+	public static void main(String[] args) {
+		String p ;
+		String a = "--admin";
+		String g = "--guest";
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+				System.out.print("Please, enter any text: ");
+				String userInput = sc.nextLine();
+		{
+			if (a == a) {
+				System.out.println("Hello, Admin!");
+			}else if(g == g) {
+				System.out.println("Hello, Guest!");
+			}
+			System.out.println("Please, select either 'ADMIN' or 'GUEST' mode for this program");
+		}
+		
+		
+
+	}
+
+}
