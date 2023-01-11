6183d1da1812bf83a4b4351f5220e7dea318c1e6
wiwin ristanto <wiwinristanto94@gmail.com>
2023-01-11 18:01:08 +0700

    menambahkan deskripsi dan membuat code AddNumbers

--- src/com/learnIT/AddNumbers.java
+++ src/com/learnIT/AddNumbers.java
@@ -1,16 +1,25 @@
 package com.learnIT;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class AddNumbers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int p = Integer.parseInt(args[1]);
-		Scanner sc = new Scanner(System.in);
-		float i = sc.nextFloat();
-		float j = sc.nextFloat();
-		System.out.println(i+j+args[0]);
+//		int p = Integer.parseInt(args[0]);
+//		Scanner sc = new Scanner(System.in);
+//		double i = sc.nextDouble();
+//		double j = sc.nextDouble();
+//		System.out.println(i+j+args[1]);
+		if (args[0].contains(".") || args[1].contains(".")) {
+			double paramDouble1 = Double.parseDouble(args[0]);
+			double paramDouble2 = Double.parseDouble(args[1]);
+			System.out.println(paramDouble1 + paramDouble2);
+		} else {
+			int paramInt1 = Integer.parseInt(args[0]);
+			int paramInt2 = Integer.parseInt(args[1]);
+			System.out.println(paramInt1 + paramInt2);
+		}
 	}
 
 }
