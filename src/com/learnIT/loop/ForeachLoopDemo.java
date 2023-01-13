c1dff4f72210ff64776760891732357a1892614f
wiwin ristanto <wiwinristanto94@gmail.com>
2023-01-13 15:34:34 +0700

    Menambah file ForEachLoopDemo

---
+++ src/com/learnIT/loop/ForeachLoopDemo.java
@@ -0,0 +1,17 @@
+package com.learnIT.loop;
+
+public class ForeachLoopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {1,2,3};
+		int sum = 0 ;
+		for (int number :arr) {
+			sum += number;
+			System.out.println("Number: "+number);
+		}
+		System.out.println("Sum: "+sum);
+
+	}
+
+}
