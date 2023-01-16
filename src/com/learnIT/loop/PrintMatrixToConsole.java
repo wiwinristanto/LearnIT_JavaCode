5bef1ec9e06921acd3fc67753edc0cb5f251dbdb
wiwin ristanto <wiwinristanto94@gmail.com>
2023-01-16 12:15:49 +0700

    membuat file matrix array didalam array

---
+++ src/com/learnIT/loop/PrintMatrixToConsole.java
@@ -0,0 +1,23 @@
+package com.learnIT.loop;
+
+public class PrintMatrixToConsole {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] matrix = {
+				{1, 2, 3, 4, 5,},
+				{6, 7},
+				{8, 9, 10},
+				{7,5,3}
+		};
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				System.err.print(matrix[i][j]+ " ");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
