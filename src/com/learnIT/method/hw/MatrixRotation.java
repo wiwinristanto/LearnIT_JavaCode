8cf0a7859c5b3b68afbc954a34d78c3a4ab20f19
wiwin ristanto <wiwinristanto94@gmail.com>
2023-03-07 18:16:15 +0700

    Update MatrixRotation

--- src/com/learnIT/method/hw/MatrixRotation.java
+++ src/com/learnIT/method/hw/MatrixRotation.java
@@ -1,10 +1,135 @@
 package com.learnIT.method.hw;
 
-public class MatrixRotation {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+public class MatrixRotation {
+    
+   public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Please, enter matrix  size: ");
+		int size = in.nextInt();
+		double[][] matrix = generateMatrix(size);
+		
+		System.out.println("How you want to rotate matrix: " + System.lineSeparator() + "\t1 - 90"
+				+ System.lineSeparator() + "\t2 - 180" + System.lineSeparator() + "\t3 - 270");
+		int mode = in.nextInt();
+		
+		System.out.println(System.lineSeparator()+ "Base matrix:"+ System.lineSeparator());
+		printMatrixToConsole(matrix);
+		System.out.println();
+		
+		if (rotateMatrix(matrix, mode)) {
+			printMatrixToConsole(matrix);
+		}
 
 	}
+	
+	private static double[][] generateMatrix(int size) {
+		// TODO Auto-generated method stub
+		double[][]matrix = new double[size][size];
+		
+		for (int i = 0; i < matrix.length; i++){
+			for (int j = 0; j < matrix.length; j++) {
+				matrix[i][j] = Double.valueOf(Integer.toString(i)+"."+Integer.toString(j));
+			}
+		}
+		return matrix;
+	}
+
+	private static boolean rotateMatrix(double[][] matrix, int mode) {
+		// TODO Auto-generated method stub
+		switch (mode) {
+		case 1:
+			System.out.println("90 degrees rotate:" + System.lineSeparator());
+			rotate90(matrix);
+			break;
+		case 2:
+			System.out.println("180 degrees rotate:" + System.lineSeparator());
+			rotate180(matrix);
+			break;
+		case 3:
+			System.out.println("270 degrees rotate:" +System.lineSeparator());
+			rotate270(matrix);
+			break;
+		default:
+			System.out.println("You selected non-existing mode!");
+			return false;
+		}
+		return true;
+	}
+
+	public static void rotate270(double[][] matrix) {
+		// TODO Auto-generated method stub
+		transposeMatrix(matrix);
+		horizontalReflection(matrix);
+		
+	}
+
+	public static void rotate180(double[][] matrix) {
+		// TODO Auto-generated method stub
+		verticalReflection(matrix);
+		horizontalReflection(matrix);
+		
+	}
+
+	public static void horizontalReflection(double[][] matrix) {
+		// TODO Auto-generated method stub
+		double temp;
+		for (int i = 0; i < matrix.length / 2; i++) {
+			for (int j = 0; j < matrix.length; j++) {
+				temp = matrix[i][j];
+				matrix[i][j] = matrix[matrix.length - 1 - i][j];
+				matrix[matrix.length - 1 - i][j] = temp;
+			}
+		}
+		
+	}
+
+	public static void rotate90(double[][] matrix) {
+		// TODO Auto-generated method stub
+		transposeMatrix(matrix);
+		verticalReflection(matrix);
+		
+	}
+
+	private static void verticalReflection(double[][] matrix) {
+		// TODO Auto-generated method stub
+		double temp;
+		for (int i = 0; i < matrix.length; i++){
+		    for (int j = 0; j < matrix.length / 2; j++){
+		        temp = matrix[i][j];
+		        matrix[i][j] = matrix[i][matrix.length - 1 - j];
+		        matrix[i][matrix.length - 1 - j] = temp;
+		    }
+		}
+		
+	}
+
+	private static void transposeMatrix(double[][] matrix) {
+		// TODO Auto-generated method stub
+		double temp;
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < i; j++) {
+				temp = matrix[i][j];
+				matrix[i][j] = matrix[j][i];
+				matrix[j][i] = temp;
+			}
+		}
+		
+	}
+
+	private static void printMatrixToConsole(double[][] matrix) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix.length; j++) {
+				System.out.print(matrix[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+	
+
 
 }
