c61e214a543b3cbd3d0c7d8a8249ad7ebe27ba0a
wiwin ristanto <wiwinristanto94@gmail.com>
2023-03-09 17:58:58 +0700

    membuat file ArrayAnonymousMethod untuk memasukan array anonim kedalam method

---
+++ src/com/learnIT/Array/ArrayAnonymousMethod.java
@@ -0,0 +1,21 @@
+package com.learnIT.Array;
+
+import javax.sql.rowset.JoinRowSet;
+
+public class ArrayAnonymousMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		printArray(new int[] {10,22,44,66});
+		
+	}
+	
+	//Java program to demonstrade the way of passing anonmous array to 
+	static void printArray(int arr[]) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+			System.out.println(arr[0]+ "a");
+		}
+	}
+
+}
