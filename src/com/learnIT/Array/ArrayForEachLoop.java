c0cf363841554042c3ba1470a0cb9c508d9d722d
wiwin ristanto <wiwinristanto94@gmail.com>
2023-03-09 17:32:26 +0700

    membuat file ArrayForEachLoop , array nakan metode for-each loop

---
+++ src/com/learnIT/Array/ArrayForEachLoop.java
@@ -0,0 +1,15 @@
+package com.learnIT.Array;
+
+public class ArrayForEachLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Java program to print the array elements using for each loop 
+		int [] arr = {33,3,4,5};
+		//printing array using for-each loop
+		for(int i:arr)
+			System.out.println(i);
+
+	}
+
+}
