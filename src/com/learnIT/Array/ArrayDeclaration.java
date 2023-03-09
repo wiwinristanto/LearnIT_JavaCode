422561f528ed6bd7610c965c02a4b84b883b1221
wiwin ristanto <wiwinristanto94@gmail.com>
2023-03-09 17:27:33 +0700

    membuat file ArrayDeclaration , mendeklarasikan array dengan menginitiasi langsung ke dalam kurawal. test sukses

---
+++ src/com/learnIT/Array/ArrayDeclaration.java
@@ -0,0 +1,17 @@
+package com.learnIT.Array;
+
+public class ArrayDeclaration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Java program to illustrate the use of declaration, instantiation
+		//and initialization of java array in a single line
+		
+		int a[] = {33,3,4,5}; //declaration, instantiation and initialization
+		//printing array
+		for (int i = 0; i < a.length; i++)//length is the property of array
+			System.out.println(a[i]);
+
+	}
+
+}
