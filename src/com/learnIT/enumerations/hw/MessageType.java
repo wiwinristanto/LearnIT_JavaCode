fcb9e9d5dc662ffeaaa9ce89f3ad43456b16019f
wiwin ristanto <wiwinristanto94@gmail.com>
2023-03-17 18:02:57 +0700

    membuat file MessageType untuk mengambil data dari Priority

--- src/com/learnIT/enumerations/hw/MessageType.java
+++ src/com/learnIT/enumerations/hw/MessageType.java
@@ -1,5 +1,16 @@
 package com.learnIT.enumerations.hw;
 
-public class MessageType {
-
+public enum MessageType {
+	A(Priority.HIGH),B(Priority.MEDIUM),C(Priority.LOW),D(Priority.LOW);
+	 private Priority priority;
+	 
+	 private MessageType(Priority priority) {
+		// TODO Auto-generated constructor stub
+		 this.priority = priority;
+	}
+	 
+	 public Priority getPriority() {
+		return this.priority;
+	}
+	 
 }
